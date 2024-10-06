bac03dbf23695d393a6b49c1c1e2e138ce9cc96d
Arjun A <forcoding35@gmail.com>
2024-10-06 23:39:00 +0530

    python-arithmetic-operators

---
+++ arithemeticoperators.py
@@ -0,0 +1,29 @@
+# arithmetic operators
+# addition
+# subtraction
+# multiplication
+# division - gives the result as a float
+# modulo
+# exponent
+# floor division - that gives the result of the division as an integer
+# order of execution of arithmetic operators :
+# B - brackets
+# E - exponent
+# D - division
+# M - multiplication
+# A - addition
+# S - subtraction
+
+a = 1
+b = 2
+print("addition", a + b)
+print("subtraction", a - b)
+print("multiplication", a * b)
+print("division", a / b)
+print("modulo", a % b)
+print("exponent", a**b)
+print("floor division", a // b)
+
+# convert string to integer and then subtract 1
+number = input("get a number : ")
+print(int(number) - 1)
