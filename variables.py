3e0473c9babe7f8374819f2bbec459a7d185d057
Arjun A <forcoding35@gmail.com>
2024-10-06 22:58:49 +0530

    python -variables

---
+++ variables.py
@@ -0,0 +1,10 @@
+# variables are used to store value
+# variable_name = value or a function 
+
+world = "world!"
+print('hello', world)
+hello = "hello"
+world = "user!"
+print(hello, world)
+hello = world
+print(hello, world)
