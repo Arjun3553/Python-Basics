9a788abcb10d920d98bb1de866f6910b63b04bd4
Arjun A <forcoding35@gmail.com>
2024-10-06 22:42:41 +0530

    python - output&printing

---
+++ outputandprinting.py
@@ -0,0 +1,5 @@
+# to output a value in python we use print()
+# every print statement ends with a newline character
+print("hello world")
+print('hello world', 4.5, True, end='\t')
+print('hello world' ,end='\t')
