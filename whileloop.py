542cd7533b29c6c50df02d3e4053c738bf152f26
Arjun A <forcoding35@gmail.com>
2024-10-07 14:32:22 +0530

    python-whileloop

---
+++ whileloop.py
@@ -0,0 +1,12 @@
+# while loop
+# ----------------------------------------------------------------
+# while loop is used to run when we don't know how many iterations are required
+
+# while condition == True / False
+i = 0
+while True:
+    print("running")
+    i += 1
+    if i == 10:
+        print("counter : ", i)
+        break
